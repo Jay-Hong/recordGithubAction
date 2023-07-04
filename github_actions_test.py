5ae67e948b852198b5b4d9ccb8b519e9c256e837
Jay Hong <37726672+Jay-Hong@users.noreply.github.com>
2023-07-04 14:17:17 +0900

    Update github_actions_test.py

--- github_actions_test.py
+++ github_actions_test.py
@@ -34,11 +34,11 @@ for i in range(len(날짜)):
         '거래량':거래량[i],
         })
 
-#파일을 한 번 쓴다.
+#파일을 한 번 쓴다
 with open('data.js', "w", encoding="UTF-8-sig") as f_write:
     json.dump(l, f_write, ensure_ascii=False, indent=4)
 
-#파일을 다시 읽는다.
+#파일을 다시 읽는다
 data = ""
 with open('data.js', "r", encoding="UTF-8-sig") as f:
     line = f.readline()
@@ -46,7 +46,7 @@ with open('data.js', "r", encoding="UTF-8-sig") as f:
         data += line
         line = f.readline()
 
-#파일에 변수명을 추가하여 다시 쓴다.
+#파일에 변수명을 추가하여 다시 쓴다
 final_data = f"var data = {data};"
 with open('data.js', "w", encoding="UTF-8-sig") as f_write:
     f_write.write(final_data)
